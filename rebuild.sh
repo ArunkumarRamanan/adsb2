ee57371639100ee8286a30234faa9a43f4985f1b
Wei Dong <wdong@wdong.org>
2016-02-11 14:59:06 -0500

    update

--- rebuild.sh
+++ rebuild.sh
@@ -4,8 +4,8 @@
 #caffe/bound_gyf/train.sh
 #cp caffe/bound_gyf/snapshots/fcn_iter_50000.caffemodel models/bound/caffe.params
 caffe/contour/import.sh
-#caffe/contour/train.sh
-#cp caffe/contour/snapshots/fcn_iter_50000.caffemodel models/contour/caffe.params
+caffe/contour/train.sh
+cp caffe/contour/snapshots/fcn_iter_50000.caffemodel models/contour/caffe.params
 
 
 
