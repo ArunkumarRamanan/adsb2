96aa0e9bcfac353ea4d87b171ae955b6d4045ff4
Wei Dong <wdong@wdong.org>
2016-03-07 08:56:09 -0500

    added comment in run-study.sh

--- run-study.sh
+++ run-study.sh
@@ -24,6 +24,24 @@ then
     exit
 fi
 
+### Model-related parameters
+### The same study directory should be run three times with different settings:
+### 1.  ./study   input output  (optimized for overall accuracy)
+### 2.  ./study --preset sys  input output  (optimized for systolic accuracy)
+### 3.  ./study --preset dia  input output  (optimized for diastolic accuracy)
+
+### Because the common computer vision part of the processing is very slow,
+### we support writing the intermediate results to a snapshot file, which is
+### used to speedup the computation of following two configurations.
+### So the process becomes
+### 1.  ./study   input output  --os snapshotfile
+### 2.  ./study --preset sys  -s input_snapshot output
+### 3.  ./study --preset dia  -s output_snapshot output
+### The order of parameters does not matter.
+
+### The output data of three configurations are stored as "sum_study", "sys" and "dia".
+###
+
 mkdir sum_study snapshot
 cat TRAIN TEST | while read a
 do
